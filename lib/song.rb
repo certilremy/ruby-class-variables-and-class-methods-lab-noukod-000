322bb5ba48d1d2bdc155b780e05f8e1956bae8d1
Certil Remy <certil@noukod.com>
2018-10-11 18:48:28 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+attr_accessor :name, :genre, :artist
+@@artists=[]
+@@genres=[]
+@@count = 0
+
+def initialize (name, genre, artist)
+  @@count +=1
+self.name = name
+self.genre = genre
+self.artist = artist
+@@artists << artist
+@@genres << genre
+end
+
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+      @@genres.uniq
+    end
+
+    def self.genre_count
+        genres_hash = Hash.new(0)
+        @@genres.each do |genre|
+          genres_hash[genre]+=1
+        end
+        genres_hash
+      end
+
+      def self.artist_count
+          artist_hash = Hash.new(0)
+          @@artists.each do |artist|
+            artist_hash[artist]+=1
+          end
+          artist_hash
+        end
+end
